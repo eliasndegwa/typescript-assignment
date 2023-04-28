1e20193c2c22f7a4f4a0b1274c313e634bb15392
eliasndegwa <eliasndegwamwai@gmail.com>
2023-04-28 11:25:01 +0300

    create class that fetches data from db.json

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,30 @@
-console
+interface forHabits{
+    id: number
+    habitName:string
+    dateStarted:number
+    daysPassed:number
+    
+}
+
+class Habits{
+    async showHabits(){
+        const response=await fetch(" http://localhost:3000/habits")
+        const habits=await response.json() as forHabits[]
+        // console.log(habits);
+        let html=''
+        habits.forEach(habit=>{
+            html+=`
+            <div class="card">
+                <i class="fa-solid fa-ban-smoking"></i>
+                <p>${habit.habitName}</p>
+                <p>${habit.dateStarted}</p>
+                <p>${habit.daysPassed}</p>
+            </div>
+            `
+        })
+        // console.log(html)
+        const cardContainer=document.querySelector(".card-container")! as HTMLDivElement
+        cardContainer.innerHTML=html
+    }
+}
+new Habits().showHabits()
